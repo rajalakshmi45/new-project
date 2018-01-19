936d9a5299c8b5fefda2ac6590ad952894d08471
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2018-01-19 16:50:19 +0530

    Update oddinterval.c

--- oddinterval.c
+++ oddinterval.c
@@ -3,9 +3,12 @@ void main()
 {
  int a,b,i;
          scanf("%d\t%d",&a,&b);
+    if(a<=100000)
+    {
          for(i=a+1;i<b;i++)
          {
          	if(i%2!=0)
          	printf("%d\t",i);
          }
+    }
  }
