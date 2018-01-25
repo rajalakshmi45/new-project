de8d85e77c186a797c66ebdc5068172293161b61
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2018-01-25 12:22:45 +0000

    Update swap of num.c

--- beginner/swap of num.c
+++ beginner/swap of num.c
@@ -4,9 +4,12 @@ int main(void) {
 	int n,m;
 	int temp;
 	scanf("%d\t%d",&n,&m);
+	if(n<100000)
+	{
 	temp=n;
 	n=m;
 	m=temp;
-	printf("%d\t%d",n,m);
+	}
+        printf("%d\t%d",n,m);
 	return 0;
 }
