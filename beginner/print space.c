3a05d313390ba6e09c04c211955625d449a24736
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2018-01-25 16:54:47 +0530

    Create print space.c

---
+++ beginner/print space.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+            char a[1000];
+            int count=0,i;
+            scanf("%[^\n]s",&a);
+            for(i=0;a[i]!='\0';i++)
+            {
+            	if(a[i]==' ')
+            	{
+            		count=count+1;
+            	}
+            }
+            printf("%d",count);
+	return 0;
+}
