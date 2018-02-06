413bfdfc9f0da8aacfee5293858f4adc2628872f
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2018-02-06 12:23:15 +0000

    Create swap of 2 num.c

---
+++ beginner/set 6/swap of 2 num.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(void) {
+  int n,m,temp;
+  scanf("%d\t%d",&n,&m);
+  if(n<=100000)
+  {
+  temp=n;
+  n=m;
+  m=temp;
+  }
+  printf("%d\t%d",n,m);
+  
+  return 0;
+}
