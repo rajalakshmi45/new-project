040087e35f110db50e373b413c05d8da4f631ed2
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2018-02-08 09:33:53 +0000

    Create vowel in word.c

---
+++ beginner/set 7/vowel in word.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+int main(void) {
+  char a[100];
+  char b[10]={'a','e','i','o','u'};
+  int i,j,count=0;
+  scanf("%s",a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    for(j=0;j<5;j++)
+    {
+      if(a[i]==b[j])
+      {
+        count++;
+      }
+    }
+  }
+  if(count>0)
+  {
+    printf("yes");
+  }
+  return 0;
+}
