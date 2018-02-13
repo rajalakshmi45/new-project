6e0540399b022ea8a19a7f32346871db04bde7e1
rajalakshmi45 <30829669+rajalakshmi45@users.noreply.github.com>
2018-02-13 08:53:55 +0000

    Create composite num.c

---
+++ beginner/set 8/composite num.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(void) {
+  int n,i,count=0;
+  scanf("%d",&n);
+  for(i=2;i<=n;i++)
+  {
+    if(n%i==0)
+    {
+      count++;
+    }
+  }
+  if(count<=1)
+  {
+    printf("no");
+  }
+  else
+  {
+    printf("yes");
+  }
+  return 0;
+}
